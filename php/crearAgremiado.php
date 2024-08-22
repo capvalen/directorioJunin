90637348c00a0b82db1f9e93ec32e44e4648ba4d
Infocat <infocat2.0@gmail.com>
2024-08-22 12:02:34 -0500

    Update crearAgremiado.php

--- php/crearAgremiado.php
+++ php/crearAgremiado.php
@@ -3,26 +3,27 @@ include "conexion.php";
 $_POST = json_decode(file_get_contents('php://input'),true); 
 $filas = array();
 
-$sqlProx="select proximoColegiado() as id;";
+/*$sqlProx="select proximoColegiado() as id;";
 $resultadoProx=$esclavo->query($sqlProx);
 if($rowProx=$resultadoProx->fetch_assoc()){
 	$proximo = $rowProx['id'];
 
-	$sql="INSERT INTO `colegiados` (`id`, `dni`, `apellidos`, `colegiatura`, `fecha`, `habilitado`, `foto`) VALUES 
-	(NULL, '{$_POST['dni']}', UPPER('{$_POST['apellidos']}'), {$proximo}, '{$_POST['fecha']}', UPPER('{$_POST['habilitado']}'), '{$_POST['foto']}');";
 	
-	if($cadena->query($sql)){
-		echo $proximo;
-	}else{
-		echo "error";
-	}
+}else{
+	echo "error";
+}*/
+
+$sql="INSERT INTO `colegiados` (`id`, `dni`, `apellidos`, `colegiatura`, `fecha`, `habilitado`, `foto`) VALUES 
+	(NULL, '{$_POST['dni']}', UPPER('{$_POST['apellidos']}'), -1, '{$_POST['fecha']}', UPPER('{$_POST['habilitado']}'), '{$_POST['foto']}');";
+	
+if($cadena->query($sql)){
+    $idColegiado =mysqli_insert_id ( $cadena); //->lastInsertId();
+    
+    $sqlUpdate = "UPDATE `colegiados` SET `colegiatura` = ${idColegiado} where `id`= ${idColegiado};";
+    $cadena->query($sqlUpdate);
+	echo $idColegiado;
 }else{
 	echo "error";
 }
-
-
-
-
-
 
 ?>
